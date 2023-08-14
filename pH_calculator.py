e3326b203d5b68503fab95be02b90d9638d1cf5a
caitlinr77 <126729801+caitlinr77@users.noreply.github.com>
2023-08-14 18:27:43 -0400

    Update pH_calculator.py

--- pH_calculator.py
+++ pH_calculator.py
@@ -2,28 +2,31 @@ import json
 import numpy as np
 import math
 
-
 def alpha():
-    with open('all_TPEN_DERIVATIVE_records.json', 'r', encoding = 'utf8') as f:
-        json_data = json.load(f)
-    
-        filtered_data = []
+    try:
+        with open('all_TPEN_DERIVATIVE_records.json', 'r', encoding = 'utf-8') as f:
+    #the load function will directly load and parse the file, creating a dictionary 
+            json_data = json.load(f)
 
-    for aqueous in json_data["aqueous phase"]:
-        aqueous_data = {
-            "solutes" == aqueous["solutes"],
-            "pH" == aqueous["pH"],
-            "volume" == aqueous["volume"],}
+            filtered_data = []
+
+            for aqueous in json_data["aqueous phase"]:
+                aqueous_data = {
+                "solutes" == aqueous["solutes"],
+                "pH" == aqueous["pH"],
+                "volume" == aqueous["volume"],}
+                            
+            filtered_data.append(aqueous_data)
                         
-    filtered_data.append(aqueous_data)
-                    
-    if isinstance("pH", (int, float)):
-        pH = np.array(pH, dtype=float)
-                        
-        h3o = -math.log10(pH)
+            if isinstance("pH", (int, float)):
+                pH = np.array("pH", dtype=float)
+                            
+            h3o = -math.log10(pH)
+            return h3o
+        print(h3o)
+            
+    except ValueError:
+        print('Decoding JSON has failed')
 
-        return h3o
-    print(h3o)
-
-alpha()
-        
+    alpha()
+   
